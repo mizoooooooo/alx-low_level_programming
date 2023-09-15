552c09896e64f4ca698e0b8e300f8d4556738b41
mizoooooooo <moaz.gold123@gmail.com>
2023-09-14 17:20:00 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  _isupper - check if c uppercase
+ *
+ *  @c: input integar type
+ *
+ *  Return: 1 if c uppercase, 0 if not
+*/
+
+int _isupper(int c)
+{
+	int val;
+
+	if (val <= 90 && val >= 65)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
