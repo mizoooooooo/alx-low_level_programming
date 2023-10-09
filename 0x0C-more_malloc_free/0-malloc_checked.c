534b79a49126ed10c6d601769c8fc4bba09c9c5c
mizoooooooo <moaz.gold123@gmail.com>
2023-10-09 15:21:28 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ *
+ * @b: num multiplay size
+ *
+ * Return: 98 if malloc fails
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = (int *)malloc(b);
+}
